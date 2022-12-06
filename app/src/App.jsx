e1d32364ebca71c503a3a2744093ab9d016b3f85
Thomas Barreto <thomi.b137@gmail.com>
2022-12-06 16:38:03 -0300

    + add menu

---
+++ app/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import { Link, Routes, Route } from 'react-router-dom'
+import './App.css'
+import { Problem } from './pages/problem1/Problem'
+
+
+const Hds1 = () => {
+  return <h1>asd</h1>
+}
+
+const Menu = () => {
+
+  const [count, setCount] = useState(0)
+
+  return <>
+    <h1>Prueba Tecnica AVE</h1>
+    <h2>Thomas Barreto</h2>
+    <Link className='link' to={"1"}>Ejercicio 1</Link>
+    <Link className='link' to={"2"}>Ejercicio 2</Link>
+    <Link className='link' to={"3"}>Ejercicio 3</Link>
+    <Link className='link' to={"4"}>Ejercicio 4</Link>
+    <Link className='link' to={"5"}>Ejercicio 5</Link>
+    <Link className='link' to={"6"}>Ejercicio 6</Link>
+    <Link className='link' to={"7"}>Ejercicio 7</Link>
+    <button onClick={() => setCount((count) => count + 1)}>
+      count is {count}
+    </button>
+  </>
+}
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="App">
+      <div className="card">
+        
+        <Routes>
+        <Route path='/' element={<Menu/>} />
+          <Route path='/1' element={<Problem />} />
+          <Route path='/2' element={<Hds1 />} />
+          <Route path='/3' element={<Hds1 />} />
+          <Route path='/4' element={<Hds1 />} />
+          <Route path='/5' element={<Hds1 />} />
+          <Route path='/6' element={<Hds1 />} />
+          <Route path='/7' element={<Hds1 />} />
+        </Routes>
+        <Link to={'/'}>volver</Link>
+      </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
---
+++ app/src/pages/problem1/Problem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Problem = () => {
+  return (
+    <div>
+      <input type={'number'}></input>
+      <input type={'number'}></input>
+      <button type={'submit'}>Multiplicar</button>
+    </div>
+  )
+}
